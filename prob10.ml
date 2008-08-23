f8b5bbcb6360f7e6181280dcd5da23ee0aecacd3
Kevin Ballard <kevin@sb.org>
2008-08-22 21:18:06 -0700

    Add solution to problem 10

---
+++ prob10.ml
@@ -0,0 +1,13 @@
+(*
+  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+  Find the sum of all the primes below two million.
+*)
+
+#load "sieve.cma"
+#load "nums.cma"
+open Big_int
+
+let sieve = Sieve.make 2000000
+let _ =
+  print_endline (string_of_big_int (Sieve.fold (fun a b -> add_big_int (big_int_of_int a) b) zero_big_int sieve))
