adb6365ab800c8fb24b6bb956b0c8025418f3139
Kevin Ballard <kevin@sb.org>
2008-08-22 18:14:11 -0700

    Extract prime_factors out of prob3 and into misc

--- misc.ml
+++ misc.ml
@@ -1,3 +1,6 @@
+#load "nums.cma"
+open Big_int
+
 let print_list f l =
   print_char '[';
   match l with
@@ -6,4 +9,11 @@ let print_list f l =
     print_string (f hd);
     List.iter (fun x -> print_string (", " ^ (f x))) tl
   end;
-  print_char ']';;
+  print_char ']'
+
+let prime_factors n =
+  let rec loop i n factors =
+    if ge_big_int i (sqrt_big_int n) then n :: factors
+    else if eq_big_int (mod_big_int n i) zero_big_int then loop (succ_big_int i) (div_big_int n i) (i :: factors)
+    else loop (succ_big_int i) n factors in
+  loop (big_int_of_int 2) n []
--- prob3.ml
+++ prob3.ml
@@ -4,15 +4,7 @@
   What is the largest prime factor of the number 600851475143 ?
 *)
 
-#load "nums.cma"
-open Big_int
-
-let prime_factors n =
-  let rec loop i n factors =
-    if ge_big_int i (sqrt_big_int n) then n :: factors
-    else if eq_big_int (mod_big_int n i) zero_big_int then loop (succ_big_int i) (div_big_int n i) (i :: factors)
-    else loop (succ_big_int i) n factors in
-  loop (big_int_of_int 2) n [];;
+#use "misc.ml";;
 
 (* the largest prime factor is always the head of the factors list *)
 
