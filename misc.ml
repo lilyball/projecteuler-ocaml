1659ddfe2dba67bb08d77fe5f5bb4579abcf997c
Kevin Ballard <kevin@sb.org>
2008-08-22 18:45:09 -0700

    Add print_int_list to misc and fix prime_factors
    prime_factors wasn't working properly when the sqrt of a num was a factor,
    or when there were multiple identical factors.

--- misc.ml
+++ misc.ml
@@ -11,9 +11,12 @@ let print_list f l =
   end;
   print_char ']'
 
+let print_int_list l =
+  print_list string_of_int l
+
 let prime_factors n =
   let rec loop i n factors =
-    if ge_big_int i (sqrt_big_int n) then n :: factors
-    else if eq_big_int (mod_big_int n i) zero_big_int then loop (succ_big_int i) (div_big_int n i) (i :: factors)
+    if gt_big_int i (sqrt_big_int n) then n :: factors
+    else if eq_big_int (mod_big_int n i) zero_big_int then loop i (div_big_int n i) (i :: factors)
     else loop (succ_big_int i) n factors in
   loop (big_int_of_int 2) n []
