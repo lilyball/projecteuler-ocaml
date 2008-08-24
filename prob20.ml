e97918e1bf0ed25081950b1beb9d939a1b2138ae
Kevin Ballard <kevin@sb.org>
2008-08-24 06:46:15 -0700

    Add solution to problem 20

---
+++ prob20.ml
@@ -0,0 +1,24 @@
+(*
+  http://projecteuler.net/index.php?section=problems&id=20
+
+  n! means n x (n - 1) x ... x 3 x 2 x 1
+
+  Find the sum of the digits in the number 100!
+*)
+
+#load "nums.cma"
+open Big_int
+
+let fact_big_int i =
+  let rec fact_big_int_aux i acc =
+    if i = 0 then acc
+    else fact_big_int_aux (pred i) (mult_int_big_int i acc) in
+  fact_big_int_aux i unit_big_int
+
+let large_number = fact_big_int 100
+
+let _ =
+  let sum = ref 0 in
+  String.iter (fun i -> sum := !sum + (int_of_char i) - (int_of_char '0'))
+    (string_of_big_int large_number);
+  print_int !sum; print_newline ()
