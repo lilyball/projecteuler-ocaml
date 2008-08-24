84b4b9dc783d4e3a9874fd7ab6e736daaa207b8b
Kevin Ballard <kevin@sb.org>
2008-08-24 03:48:18 -0700

    Add solution to problem 16

---
+++ prob16.ml
@@ -0,0 +1,21 @@
+(*
+  http://projecteuler.net/index.php?section=problems&id=16
+  
+  2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+  What is the sum of the digits of the number 2**1000?
+*)
+
+#load "nums.cma"
+open Big_int
+
+let large_num = power_int_positive_int 2 1000
+
+let sum_digits n =
+  let sum = ref 0 in
+  String.iter (fun c -> sum := !sum + (int_of_char c) - (int_of_char '0')) (string_of_big_int n);
+  !sum
+
+let _ =
+  print_int (sum_digits large_num);
+  print_newline ()
