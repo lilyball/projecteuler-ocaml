087c0b4353ee4115f1601c1897ad93412a0c12fb
Muhammed Alkan <dev.mhlkan@gmail.com>
2019-01-15 04:38:32 -0500

    A simpler and functionl version for Problem 14

--- prob14.ml
+++ prob14.ml
@@ -14,46 +14,24 @@
   NOTE: Once the chain starts the terms are allowed to go above one million.
 *)
 
-open Big_int
-
-module BigIntHashedType =
-struct
-  type t = big_int
-  let equal = eq_big_int
-  let hash n = Hashtbl.hash (int_of_big_int (mod_big_int n (big_int_of_int max_int)))
-end
-
-module BigIntHashtbl = Hashtbl.Make(BigIntHashedType)
-
-let cache = BigIntHashtbl.create 1_000_000
-let _ =
-  (* the sequence always ends at 1 *)
-  BigIntHashtbl.add cache unit_big_int []
-
-let two_big_int = (big_int_of_int 2)
-
-let is_even n =
-  eq_big_int (mod_big_int n two_big_int) zero_big_int
-
-let calc_seq n =
-  let rec calc_seq_aux n l =
-    if BigIntHashtbl.mem cache n then
-      let seq = BigIntHashtbl.find cache n in
-      List.fold_left (fun seq n -> BigIntHashtbl.replace cache n seq; n :: seq) (n :: seq) l
+let rec collatz n =
+  if n != 1 then
+    if (n mod 2) = 0 then
+      1 + collatz (n / 2)
     else
-      let n2 = (if is_even n then div_big_int n two_big_int else add_int_big_int 1 (mult_int_big_int 3 n)) in
-      calc_seq_aux n2 (n :: l) in
-  calc_seq_aux n []
+      1 + collatz (3 * n + 1)
+  else 1
 
-let _ =
-  let m = ref 0 and n = ref 0 in
-  for i = 1 to 999_999 do
-    if (i mod 500) = 0 then (print_int i; print_newline ());
-    let seq = calc_seq (big_int_of_int i) in
-    let num_seq = List.length seq in
-    if num_seq > !m then begin
-      m := num_seq;
-      n := i
-    end
-  done;
-  Printf.printf "%d: %d\n" !n !m
+let loop n =
+  let rec loop n m =
+    if n = 0 then
+      m
+    else
+      if (collatz n) > m then
+        loop (n - 1) (collatz n)
+      else
+        loop (n - 1) m
+  in loop n 0
+
+let () =
+  print_int (loop 999999)
